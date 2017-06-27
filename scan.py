1a81ff884fa9ff03cbe55544d320f60f79f16da1
l3uddz <l3uddz@gmail.com>
2017-06-27 16:58:05 +0100

    use webhook instead of custom script

--- scan.py
+++ scan.py
@@ -6,7 +6,6 @@ import sys
 import uuid
 from logging.handlers import RotatingFileHandler
 
-import requests
 from flask import Flask
 from flask import abort
 from flask import request
@@ -57,11 +56,7 @@ base_config = {
         '/mnt/unionfs': [
             '/home/seed/media/fused'
         ]
-    },
-    'SERVER_PUSH_URL': [
-        'http://localhost:3467/push'
-    ],
-    'USE_SERVER_PUSH': False
+    }
 }
 config = None
 config_path = os.path.join(os.path.dirname(sys.argv[0]), 'config.json')
@@ -112,8 +107,6 @@ def radarr(path):
     section = get_plex_section(path)
     if section <= 0:
         return
-    if config['USE_SERVER_PUSH']:
-        return push_to_server(path, 'radarr')
 
     logger.info("Scanning '%s'", path)
     plex(path, section)
@@ -123,8 +116,6 @@ def sonarr(path):
     section = get_plex_section(path)
     if section <= 0:
         return
-    if config['USE_SERVER_PUSH']:
-        return push_to_server(path, 'sonarr')
 
     logger.info("Scanning '%s'", path)
     plex(path, section)
@@ -133,12 +124,12 @@ def sonarr(path):
 def plex(path, id):
     if os.name == 'nt':
         final_cmd = '""%s" --scan --refresh --section %s --directory "%s""' \
-                    % (config['PLEX_SCANNER'], str(id), os.path.dirname(path))
+                    % (config['PLEX_SCANNER'], str(id), path)
     else:
         cmd = 'export LD_LIBRARY_PATH=' + config['PLEX_LD_LIBRARY_PATH'] \
               + ';export PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR=' \
               + config['PLEX_SUPPORT_DIR'] + ';' + config['PLEX_SCANNER'] + ' --scan --refresh --section ' \
-              + str(id) + ' --directory \\"' + os.path.dirname(path) + '\\"'
+              + str(id) + ' --directory \\"' + path + '\\"'
         final_cmd = 'sudo -u %s bash -c "%s"' % (config['PLEX_USER'], cmd)
     return os.system(final_cmd)
 
@@ -150,21 +141,6 @@ def get_plex_section(path):
                 return section
     logger.debug("Unable to map '%s' to a section id....", path)
     return -1
-
-
-def push_to_server(path, path_type):
-    for server in config['SERVER_PUSH_URL']:
-        logger.debug("Pushing '%s' to server: '%s'", path, server)
-        payload = {
-            'type': path_type,
-            'path': path,
-            'pass': config['SERVER_PASS']
-        }
-        resp = requests.post(server, payload)
-        if resp.status_code == 200 and resp.text == "OK":
-            logger.debug("Server accepted push")
-        else:
-            logger.debug("Server declined push with status code: %d", resp.status_code)
 
 
 def map_pushed_path(path):
@@ -183,25 +159,25 @@ def map_pushed_path(path):
 app = Flask(__name__)
 
 
-@app.route("/push", methods=['POST'])
+@app.route("/%s" % config['SERVER_PASS'], methods=['POST'])
 def client_pushed():
-    if 'pass' not in request.form or 'path' not in request.form or 'type' not in request.form:
-        logger.debug("Invalid push request from: %r", request.remote_addr)
-        return abort(400)
-    if request.form['type'] != 'sonarr' and request.form['type'] != 'radarr':
-        logger.debug("Invalid push request type: '%s' from: %r", type, request.remote_addr)
-        return abort(400)
-    if request.form['pass'] != config['SERVER_PASS']:
-        logger.debug("Unauthorized push request from: %r", request.remote_addr)
-        return abort(401)
-    logger.debug("Client: %r requested scan: '%s', type: '%s'", request.remote_addr, request.form['path'],
-                 request.form['type'])
+    data = request.get_json(silent=True)
+    if not data:
+        logger.debug("Invalid scan request from: %r", request.remote_addr)
+        abort(400)
 
-    final_path = map_pushed_path(request.form['path'])
-    if request.form['type'] == 'sonarr':
+    if 'Movie' in data:
+        logger.debug("Client %r scan request for movie: %s", request.remote_addr, data['Movie']['FilePath'])
+        final_path = map_pushed_path(data['Movie']['FilePath'])
+        radarr(os.path.dirname(final_path))
+    elif 'Series' in data:
+        logger.debug("Client %r scan request for series: %s", request.remote_addr, data['Series']['Path'])
+        final_path = map_pushed_path(data['Series']['Path'])
         sonarr(final_path)
     else:
-        radarr(final_path)
+        logger.debug("Unknown scan request from: %r", request.remote_addr)
+        abort(400)
+
     return "OK"
 
 
@@ -211,7 +187,7 @@ def client_pushed():
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        logger.error("You must pass an argument, sonarr, radarr, sections or server...")
+        logger.error("You must pass an argument, sections or server...")
         sys.exit(0)
     else:
         if sys.argv[1].lower() == 'sections':
@@ -224,22 +200,10 @@ if __name__ == "__main__":
                 final_cmd = 'sudo -u %s bash -c "%s"' % (config['PLEX_USER'], cmd)
             os.system(final_cmd)
 
-        elif sys.argv[1].lower() == 'radarr':
-            file_path = os.environ.get('radarr_moviefile_path')
-            if file_path:
-                radarr(file_path)
-            else:
-                logger.error("Problem retrieving radarr_moviefile_path")
-
-        elif sys.argv[1].lower() == 'sonarr':
-            file_path = os.environ.get('sonarr_episodefile_path')
-            if file_path:
-                sonarr(file_path)
-            else:
-                logger.error("Problem retrieving sonarr_episodefile_path")
         elif sys.argv[1].lower() == 'server':
-            logger.debug("Starting server: http://%s:%d/push", config['SERVER_IP'], config['SERVER_PORT'])
+            logger.debug("Starting server: http://%s:%d/%s", config['SERVER_IP'], config['SERVER_PORT'],
+                         config['SERVER_PASS'])
             app.run(host=config['SERVER_IP'], port=config['SERVER_PORT'], debug=False, use_reloader=False)
             logger.debug("Server stopped")
         else:
-            logger.error("You must pass an argument of either sonarr, radarr, sections or server...")
+            logger.error("You must pass an argument of either sections or server...")
