45d7d879dc0701e25b929ea9705797c1430bb464
m1lkman <10157301+m1lkman@users.noreply.github.com>
2019-05-25 10:58:49 -0700

    Update rclone.py
    -Updated Rclone crypt decoder to better support crypts existing in subfolders and on Team drives
    -changed references of "decrypt" to "decode"

--- rclone.py
+++ rclone.py
@@ -4,40 +4,39 @@ import logging
 
 logger = logging.getLogger("RCLONE")
 
-class RcloneDecrypter:
-    def __init__(self,binary,crypt_mappings,config):
+class RcloneDecoder:
+    def __init__(self, binary, crypt_mappings, config):
         self._binary = binary
         if self._binary == "" or not os.path.isfile(binary):
              self._binary = os.path.normpath(subprocess.check_output(["which", "rclone"]).decode().rstrip('\n'))
-             logger.info("Rclone binary path located as {}".format(binary))
- 
-        self._config = config 
+             logger.info("Rclone binary path located as '{}'".format(binary))
+
+        self._config = config
         self._crypt_mappings = crypt_mappings
 
-    def decrypt_path(self, path):
-        # Isolate root/file path and attempt to locate entry in mappings
-        paths = path.split(os.sep,1)
-        root_dir = paths[0]
-        file_path = paths[1]
-        logger.info("Root directory identified as {}".format(root_dir))
+    def decode_path(self, path):
+        for crypt_dir, mapped_remotes in self._crypt_mappings.items():
+            # Isolate root/file path and attempt to locate entry in mappings
+            logger.info("Crypt base directory identified as '{}'".format(crypt_dir))
+            file_path = path.replace(crypt_dir,'')
+            logger.info("Encoded file Path identified as '{}'".format(file_path))
+            if path.lower().startswith(crypt_dir.lower()):
+                for mapped_remote in mapped_remotes:
+                    logger.info("Crypt base directory '{}' has mapping defined in config as remote '{}'. Attempting to decode".format(crypt_dir, mapped_remote))
+                    logger.debug("Raw query is '{}'".format(" ".join([self._binary, "cryptdecode", mapped_remote, file_path])))
+                    try:
+                        decoded = subprocess.check_output([self._binary, "--config", self._config, "cryptdecode", mapped_remote, file_path], stderr=subprocess.STDOUT).rstrip('\n')
+                    except subprocess.CalledProcessError as e:
+                        logger.error("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+                        return None
 
-        if root_dir  in self._crypt_mappings:
-            logger.info("Root directory {} has mapping defined in config as remote {}. Attempting to decrypt".format(root_dir,self._crypt_mappings[root_dir]))
-            logger.info("Raw query is {}".format(" ".join([self._binary,"cryptdecode",self._crypt_mappings[root_dir],file_path])))
-            try:
-                 decoded = subprocess.check_output([self._binary,"--config",self._config,"cryptdecode",self._crypt_mappings[root_dir],file_path],stderr=subprocess.STDOUT).decode().rstrip('\n')
-            except subprocess.CalledProcessError as e:
-                 logger.error("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
-                 return None
-            
-            decoded = decoded.split(' ',1)[1].lstrip()
-            
-            if "failed" in decoded.lower():
-                 logger.error("Failed to decode path {}".format(file_path))
-            else: 
-                 logger.info("Deccoded path of {} is {}".format(file_path,decoded))
-                 return [os.path.join(root_dir,decoded)]
+                    decoded = decoded.split(' ',1)[1].lstrip()
+
+                    if "failed" in decoded.lower():
+                        logger.error("Failed to decode path '{}'".format(file_path))
+                    else:
+                        logger.info("Decoded path of '{}' is '{}'".format(file_path, decoded))
+                        return [os.path.join(crypt_dir, decoded.decode('utf-8'))]
+            else:
+                logger.debug("Ignoring crypt decode for path '%s' because '%s' was not matched from CRYPT_MAPPING", path, crypt_dir)
         return None
-
-
-
