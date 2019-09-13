dec63dac0962bafd9be461dcfe38fb9696e5d962
davemaster223 <16483722+daghaian@users.noreply.github.com>
2019-09-13 10:39:53 -0700

    Added config flag to debug statement

--- rclone.py
+++ rclone.py
@@ -23,9 +23,9 @@ class RcloneDecoder:
                 for mapped_remote in mapped_remotes:
                     logger.info("Crypt base directory identified as '{}'".format(crypt_dir))
                     logger.info("Crypt base directory '{}' has mapping defined in config as remote '{}'. Attempting to decode".format(crypt_dir, mapped_remote))
-                    logger.debug("Raw query is '{}'".format(" ".join([self._binary, "cryptdecode", mapped_remote, file_path])))
+                    logger.debug("Raw query is '{}'".format(" ".join([self._binary, "--config", self._config, "cryptdecode", mapped_remote, file_path])))
                     try:
-                        decoded = subprocess.check_output([self._binary, "--config", self._config, "cryptdecode", mapped_remote, file_path], stderr=subprocess.STDOUT).decode('utf-8').rstrip('\n')
+                        decoded = subprocess.check_output([self._binary, "--config", self._config, "cryptdecode", mapped_remote, file_path], stderr=subprocess.STDOUT).decode('utf-8').git rstrip('\n')
                     except subprocess.CalledProcessError as e:
                         logger.error("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
                         return None
