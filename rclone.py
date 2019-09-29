e7e9a6f54a0272d6b60d5c4655704e7bfedeb326
desimaniac <5501908+desimaniac@users.noreply.github.com>
2019-09-29 16:00:53 -0500

    test

--- rclone.py
+++ rclone.py
@@ -18,7 +18,7 @@ class RcloneDecoder:
         for crypt_dir, mapped_remotes in self._crypt_mappings.items():
             # Isolate root/file path and attempt to locate entry in mappings
             file_path = path.replace(crypt_dir,'')
-            logger.info("Encoded file path identified as u'{}'".format(file_path.decode()))
+            logger.info("Encoded file path identified as u'{}'".format(file_path.encode('utf-8', 'ignore').decode('utf-8'))
             if path.lower().startswith(crypt_dir.lower()):
                 for mapped_remote in mapped_remotes:
                     logger.info("Crypt base directory identified as u'{}'".format(crypt_dir))
