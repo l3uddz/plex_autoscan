e93c62e50f20315aa850fb7a21120f3be9b3db3d
desimaniac <5501908+desimaniac@users.noreply.github.com>
2019-10-30 17:37:41 -0500

    minor tweak

--- utils.py
+++ utils.py
@@ -27,18 +27,17 @@ def get_plex_section(config, path):
             conn.text_factory = str
             with closing(conn.cursor()) as c:
                 # check if file exists in plex
-                logger.debug("Checking if root folder path '%s' matches Plex Library root path in the Plex DB at '%s'",
-                             path, config['PLEX_DATABASE_PATH'])
+                logger.debug("Checking if root folder path '%s' matches Plex Library root path in the Plex DB.", path)
                 section_data = c.execute("SELECT library_section_id,root_path FROM section_locations").fetchall()
                 for section_id, root_path in section_data:
                     if path.startswith(root_path + os.sep):
-                        logger.debug("Plex Library Section ID '%s' matching root folder '$s' was found in the Plex DB "
-                                     "'sections_locations' table.", section_id, root_path)
+                        logger.debug("Plex Library Section ID '%s' matching root folder '$s' was found in the Plex DB.",
+                                     section_id, root_path)
                         return int(section_id)
                 logger.error("Unable to map '%s' to a Section ID.", path)
 
     except Exception:
-        logger.exception("Exception while trying to map '%s' to a Section ID in the Plex database: ", path)
+        logger.exception("Exception while trying to map '%s' to a Section ID in the Plex DB: ", path)
     return -1
 
 def map_pushed_path(config, path):
