bd4f4d65aea06ee5549fea67a1366ecb329276ae
jolbol1 <github@jamesshopland.com>
2020-12-01 00:11:10 +0000

    Update plex.py
    Allow for scan parameters to be entered into commands

--- plex.py
+++ plex.py
@@ -153,8 +153,9 @@ def scan(config, lock, path, scan_for, section, scan_type, resleep_paths, scan_t
 
         # run external command before scan if supplied
         if len(config['RUN_COMMAND_BEFORE_SCAN']) > 2:
+            extCmd = config['RUN_COMMAND_BEFORE_SCAN'].format(config = config, lock = lock, path = path, scan_for = scan_for, section = section, scan_type = scan_type, resleep_paths = resleep_paths, scan_title = scan_title, scan_lookup_type = scan_lookup_type, scan_lookup_id = scan_lookup_id)
             logger.info("Running external command: %r", config['RUN_COMMAND_BEFORE_SCAN'])
-            utils.run_command(config['RUN_COMMAND_BEFORE_SCAN'])
+            utils.run_command(extCmd)
             logger.info("Finished running external command.")
 
         # wait for Plex to become responsive (if PLEX_CHECK_BEFORE_SCAN is enabled)
@@ -216,8 +217,9 @@ def scan(config, lock, path, scan_for, section, scan_type, resleep_paths, scan_t
 
         # run external command after scan if supplied
         if len(config['RUN_COMMAND_AFTER_SCAN']) > 2:
+            extCmd = config['RUN_COMMAND_AFTER_SCAN'].format(config = config, lock = lock, path = path, scan_for = scan_for, section = section, scan_type = scan_type, resleep_paths = resleep_paths, scan_title = scan_title, scan_lookup_type = scan_lookup_type, scan_lookup_id = scan_lookup_id)
             logger.info("Running external command: %r", config['RUN_COMMAND_AFTER_SCAN'])
-            utils.run_command(config['RUN_COMMAND_AFTER_SCAN'])
+            utils.run_command(extCmd)
             logger.info("Finished running external command.")
 
     except Exception:
